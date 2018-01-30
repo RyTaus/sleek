9f2aeb93302dc6cf47fd22578c5d741b4e5bf77c
RyTaus <rtaus1@lion.lmu.edu>
2018-01-29 21:51:36 -0800

    decided to be a good person and set up testing framework, tests to be added in next commits unless i turn to the dark side

---
+++ tests/test.js
@@ -0,0 +1,13 @@
+const { describe, it } = require('mocha');
+const assert = require('assert');
+
+const Pin = require('./../src/visual/pin.js');
+
+
+describe('Pin', () => {
+  describe('test', () => {
+    it('should work', () => {
+      assert.equal('hi', 'hi');
+    });
+  });
+});
